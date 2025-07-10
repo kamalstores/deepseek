2eb8bc15e33aa5fe3488d76d951ecc7b7f5a678f
Kamal Sharma <kamalsnitkkr@gmail.com>
2025-07-10 18:45:59 +0530

    Add some elements in webpage like sidebar and prompt

---
+++ ReadMeFirst.md
@@ -0,0 +1,74 @@
+Here we are making a AI Chatbot.
+
+So follow these phases and steps
+
+
+Run this command in terminal "npx create-next-app@latest"
+
+We are using 
+ESLint because it is a static code analysis tool for identifying problematic patterns found in JavaScript code
+Tailwind CSS for styling
+We can store files separate from src as it just one or two folder
+App Router as it provides a more structured and efficient way to manage routing, layouts, and data fetching
+Turbopack because for its speed and efficiency, particularly in development mode, for large-scale JavaScript and TypeScript applications
+Not use import alias as we can deal with relative paths
+
+################################################
+
+We can assets folder for useful things like photo and fevicon etc.
+As we have own fevicon so we replace with default one
+We make assets.js file and make a object for store files and export as assets to use in future directly
+
+Now we add own title and description in metadata object in layot.js file and also change the font to Inter
+
+####################################################
+
+Remove unnecessary div in page.js file and also remove the image Import as we don't need this
+As we want to style on own so remove default styling
+
+In global css we add theme(primary color) and smooth scroll effect in html and remove scrollbar from page to look asthetic
+
+##################################################
+
+We make some folders
+components --> It is used to store the components for project
+context --> It is used to create context api to store variable and function that is used in multiple componets 
+config --> It is used to store the configuration files like MongoDB etc.
+models --> It is used to create model files because from these model we store the data in mongoDB database 
+.env -->   It is used to store the environment variables
+
+##################################################
+Its time to add some elements in home page
+
+So in page.js
+we create some state variables like
+for expand or collapse sidebar ---> we initilaze by false
+for messages --> we initiliaze with empty array
+for Loading --> we initilaze by false
+
+Now we add div and within this
+we make another div for sidebar and main content and within this div we made another div to group two images(menu icon and chat icon) and add some css
+And in menu icon we add on-click property to setExpand make false if already expand otherwise true
+
+and we check messages length as it is array and apply like if else condition,  if meassgae is empty we add image(logo) and p tag
+
+Covert name to jsx as we want to return html
+
+then a p for telling reference etc 
+
+#####################################################
+
+In comonents folder
+we make some files
+Sidebar jsx --> for sidebar element 
+PromptBox jsx --> for prompt
+
+
+######################################################
+In sidebar jsx
+Add elemets like open and closed sidebar, button for new chat  , a image when message is there or another image , when msg not there etc
+and some property of css
+
+
+######################################################
+In promptbox jsx
---
+++ components/Sidebar.jsx
@@ -0,0 +1,81 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Sidebar = ({expand,setExpand}) => {
+    return (
+        <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
+            <div>
+                <div className={`flex ${expand ? "flex-row gap-10" : "flex-col items-center gap-8"}`}>
+                    {/* when sidebar is small we show only logo */}
+                    <Image className={expand ? "w-36" : "w-10"} src = {expand ? assets.logo_text : assets.logo_icon} alt=''/>
+
+                    <div onClick={() => expand ? setExpand(false) : setExpand(true)} className="group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer">
+                        {/* it will be hidden on medium and its above sizes */}
+                        <Image className="md:hidden" src = {assets.menu_icon} alt=''/>
+
+                        {/* it will be hidden on medium and its above sizes and add block so it will be visual*/}
+                        <Image className="hidden md:block w-7" src = {expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt=''/>
+
+                        <div className={`absolute w-max ${expand ? "left-1/2 -translate-x-1/2 top-12" : "-top-12 left-0"} opacity-0 group-hover:opacity-100 transition ng-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
+                            {expand ? 'Close sidebar' : 'Open sidebar'}
+                            <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "left-1/2 -top-1.5 -translate-x-1/2" : "left-4 -bottom-1.5"}`}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Add button for chat */}
+                <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? "bg-primary hover:opacity-90 rounded-2xl gap-2 p-.5 w-max" : "group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+                    <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=""/>
+
+                    {/* Add text of new chat  */}
+                    <div className="absolute w-max -top-1 -right-1 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none">
+                        New chat
+                        <div className="w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5"></div>
+                    </div>
+
+                    {/* Add some text  */}
+                    {expand && <p className="text-white text font-medium">New chat</p>}
+
+                </button>
+
+                {/* For messages  */}
+                <div className={`mt-8 text-white/5 text-sm ${expand ? "block" : "hidden"}`}>
+                    <p className="my-1">Recents</p>
+                    
+                    {/* When prompt comes so for label name  */}
+                    {/* chatlabel  */}
+
+                </div>
+
+
+            </div>
+
+            {/* Profile icon , login link etc */}
+            <div>
+                <div className={`flex items-center cursor-pointer group relative ${expand ? "gap-1 text-white/80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/10 cursor-pointer" : "h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+                    <Image className={expand ? "w-5" : "w-6.5 mx-auto"} src={expand ? assets.phone_icon : assets.phone_icon_dull} alt=""/>
+                    {/* Barcode  */}
+                    <div className={`absolute -top-60 pb-8 ${!expand && "-right-40"} opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
+                        <div className="relative w-max bg-black text-white text-sm p-3 rounded-lg shadow-lg">
+                        <Image src={assets.qrcode} alt='' className="w-44"/>
+                        <p>Scan to get DeepSeek App</p>
+                        <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
+                        </div>
+                    </div>
+                    {expand && <><span>Get App</span> <Image alt="" src={assets.new_icon}/></>}
+                </div>
+            </div>
+            {/* User icon and profile  */}
+            <div className={`flex items-center ${expand ? "hover:bg-white/10 rounded-lg" : "justify-center w-full"} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
+                <Image src={assets.profile_icon} alt="" className="w-7" />
+                {expand && <span>My Profile</span>}
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Sidebar
